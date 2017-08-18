911e4bb233932dc7c6475584f7ea5c6398b96255
amnotafraid <amnotafraid@gmail.com>
2017-08-18 11:18:05 -0500

    socket, generate code, url in shared/stateInit.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
 import { PhotoMgrComponent } from './photo-mgr/photo-mgr.component';
+import { ProductComponent } from './product/product.component';
+import { SyncComponent } from './sync/sync.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+
+import { SocketService } from './socket.service';
+import { stateInit } from './shared/stateInit';
+
+const config: SocketIoConfig = {
+    url: stateInit.socket_url,
+    options: {}
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     PhotoMgrComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    SyncComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    SocketIoModule.forRoot(config),
   ],
-  providers: [],
+  providers: [
+    SocketService, 
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/stateInit.ts
+++ src/app/shared/stateInit.ts
@@ -2,5 +2,6 @@ export const stateInit = {
   customer_present: false,
   product_available: false,
   paired_to_phone: false,
-  photos:[]
+  photos:[],
+  socket_url: 'ec2-54-221-218-6.compute-1.amazonaws.com:4000',
   }
