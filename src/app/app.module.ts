5d6840dfbfeedaad68bea6ff0ac1cdde19c1fbf2
amnotafraid <amnotafraid@gmail.com>
2017-08-25 16:36:34 -0500

    Revert "sync component talks to socket.io"
    This reverts commit 69c70f219f05a0cc3916557053c695038e32fd0e.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { NgModule } from '@angular/core';
-import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
+import { SyncComponent } from './sync/sync.component';
 import { PhotoShareComponent } from './photo-share/photo-share.component';
 import { PurchaseComponent } from './purchase/purchase.component';
-import { SyncComponent } from './sync/sync.component';
-
-import { SocketService } from './socket.service';
-
-const config: SocketIoConfig = {
-    url: 'ec2-54-221-218-6.compute-1.amazonaws.com:4000',
-    options: {}
-};
 
 @NgModule({
   declarations: [
     AppComponent,
-    PhotoShareComponent,
-    PurchaseComponent,
     SyncComponent,
+    PhotoShareComponent,
+    PurchaseComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    SocketIoModule.forRoot(config),
+    HttpModule
   ],
-  providers: [
-    SocketService,
-  ],
-  bootstrap: [
-    AppComponent,
-  ]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
